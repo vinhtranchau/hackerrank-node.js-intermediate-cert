1775b84329d0c7db20d29a546d9a906932ca8a8e
vinhtranchau <90888370+vinhtranchau@users.noreply.github.com>
2023-07-24 11:15:11 +0200

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,24 @@
+const ROLES_FILE = __dirname + '/roles.txt';
+const fs = require('fs');
+
+module.exports = (scope) => (req, res, next) => {
+    const userRole = req.headers['x-role'];
+
+    const rolesScopesMappingJson = fs.readFileSync(ROLES_FILE, 'utf-8').trim();
+    const rolesScopesMapping = JSON.parse(rolesScopesMappingJson);
+
+    const userScopes = rolesScopesMapping.find(roleScope => roleScope.role === userRole)?.scopes;
+    
+    if (!userScopes) {
+        return res.status(403).send('Access denied: Invalid role');
+    }
+
+    const [scopeName, actionName] = scope.split('.');
+    const allowedActions = userScopes[scopeName];
+
+    if (allowedActions && allowedActions.includes(actionName)) {
+        next();
+    } else {
+        return res.status(403).send('Access denied: Insufficient permissions');
+    }
+};
